732e211770b01ede12f24b8fd7327d35db182fbc
Akshay3237 <akshaygohel364@gmail.com>
2025-07-04 21:45:46 +0530

    pass test case of Allow the add method to handle any amount of numbers.

--- index.js
+++ index.js
@@ -3,33 +3,19 @@ class StringCalculator{
     add(numbers){
         if(numbers=="") return 0;
         numbers=numbers.split(",");
-        if(numbers.length==1){
-            if(!isNaN(parseInt(numbers))){
-                return parseInt(numbers);
+
+        let sum=0;
+        for(let i=0;i<numbers.length;i++){
+            if(!isNaN(parseInt(numbers[i]))){
+                sum+= parseInt(numbers[i]);
             }
             else{
                 throw new Error("Non-numeric is not allowed!");
             }
         }
-        else if(numbers.length==2){
-            let number1=numbers[0];
-            let number2=numbers[1];
-            let sum=0;
-            if(!isNaN(parseInt(number1))){
-                sum+= parseInt(number1);
-            }
-            else{
-                throw new Error("Non-numeric is not allowed!");
-            }
-            if(!isNaN(parseInt(number2))){
-                sum+= parseInt(number2);
-            }
-            else{
-                throw new Error("Non-numeric is not allowed!");
-            }
 
             return sum;
-        }
+        
         
     }
 }
