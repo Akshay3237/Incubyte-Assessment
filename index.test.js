2c69dcc0d459d0b1b5102a6608c918644991183c
Akshay3237 <akshaygohel364@gmail.com>
2025-07-04 22:33:54 +0530

    write test case :-Allow the add method to handle new lines between numbers

--- index.test.js
+++ index.test.js
@@ -18,18 +18,25 @@ test('only one numeric value pass as string', () => {
 test('for non numeric values it will raise excpetion',()=>{
     const calc=new StringCalculator();
     expect(()=> calc.add("a") ).toThrow("Non-numeric is not allowed!");
-})
+});
 
 test("input as two values with comma seperation",()=>{
     const calc=new StringCalculator();
     numbers="12,24";
     const sum=calc.add(numbers);
     expect(sum).toBe(36);
-})
+});
 
 test("Allow the add method to handle any amount of numbers" ,()=>{
-     const calc=new StringCalculator();
+    calc=new StringCalculator();
     numbers="12,24,25,50,4333";
     const sum=calc.add(numbers);
     expect(sum).toBe(4444);
-})
+});
+
+test("Allow the add method to handle new lines between numbers",()=>{
+    calc=new StringCalculator();
+    numbers="12,24/n25,50/n4333";
+    const sum=calc.add(numbers);
+    expect(sum).toBe(4444);
+});
