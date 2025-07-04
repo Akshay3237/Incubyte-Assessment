0c737a4423b48afe6eb87eb9033eb40679bc1fa5
Akshay3237 <akshaygohel364@gmail.com>
2025-07-04 23:46:30 +0530

    pass testcae of Support different delimiters

--- index.js
+++ index.js
@@ -2,19 +2,35 @@
 class StringCalculator{
     add(numbers){
         if(numbers=="") return 0;
-        numbers=numbers.split(/[\n,]/);
+        let delimiters = "\n,"; //default delimiters
 
-        let sum=0;
+        if (numbers.startsWith("//") && numbers[3] === '\n') {
+            delimiters += numbers[2]; //find delimiter here
+            numbers = numbers.substring(4); //changing position of numbers to avoid first 3 characters
+        }
+
+       //adding escap characters to each
+        const escapedDelimiters = delimiters
+        .split('')
+        .map(c => '\\' + c) 
+        .join('');
+
+        // create RegExp from the dynamic string because we can not directly use variable
+        const delimiterRegex = new RegExp(`[${escapedDelimiters}]`);
+
+        // now split using regex 
+        numbers = numbers.split(delimiterRegex); //here we can't split directly by taking all delimiter inside a string we need object of RegExp
+        let sum=0; //for initial value of sum as zero
         for(let i=0;i<numbers.length;i++){
-            if(!isNaN(parseInt(numbers[i]))){
-                sum+= parseInt(numbers[i]);
+            if(!isNaN(parseInt(numbers[i]))){ //check if non numeric values 
+                sum+= parseInt(numbers[i]); //do addition of each values
             }
             else{
-                throw new Error("Non-numeric is not allowed!");
+                throw new Error("Non-numeric is not allowed!"); //if non numeric values then raise Exception
             }
         }
 
-            return sum;
+            return sum; //return final sum
     }
 }
 
