1af7791806947ff9d24c60ae7c9c39333de7182a
Akshay3237 <akshaygohel364@gmail.com>
2025-07-05 06:27:04 +0530

    logically describes my testcase suite updation

--- index.test.js
+++ index.test.js
@@ -1,70 +1,80 @@
-const StringCalculator=require('./index')
+const StringCalculator = require('./index')
+describe("Beginner's testsuite", () => {
+    test('first test case with empty string', () => {
+        const calc = new StringCalculator();
+        numbers = "";
+        const sum = calc.add(numbers);
+        expect(sum).toBe(0);
+    });
 
-test('first test case with empty string', () => {
-    const calc=new StringCalculator();
-    numbers="";
-    const sum=calc.add(numbers);
-    expect(sum).toBe(0);
+
+    test('only one numeric value pass as string', () => {
+        const calc = new StringCalculator();
+        numbers = "12";
+        const sum = calc.add(numbers);
+        expect(sum).toBe(12);
+    });
+
+    test('for non numeric values it will raise excpetion', () => {
+        const calc = new StringCalculator();
+        expect(() => calc.add("a")).toThrow("Non-numeric is not allowed!");
+    });
+
+    test("input as two values with comma seperation", () => {
+        const calc = new StringCalculator();
+        numbers = "12,24";
+        const sum = calc.add(numbers);
+        expect(sum).toBe(36);
+    });
+
+    test("Allow the add method to handle any amount of numbers", () => {
+        calc = new StringCalculator();
+        numbers = "12,24,25,50,4333";
+        const sum = calc.add(numbers);
+        expect(sum).toBe(4444);
+    });
+
+    test("Allow the add method to handle new lines between numbers", () => {
+        calc = new StringCalculator();
+        numbers = "12,24\n25,50\n4333";
+        const sum = calc.add(numbers);
+        expect(sum).toBe(4444);
+    });
+
+    /*
+    To change the delimiter, the beginning of the string will contain 
+    a separate line that looks like this: "//[delimiter]\n[numbers…]".
+    //  For example, "//;\n1;2" where the delimiter is ";"
+    //  should return 3.
+    */
+
+
+    test("Support different delimiters:", () => {
+        calc = new StringCalculator();
+        numbers = "//;\n12,24;25,50\n4333";
+        const sum = calc.add(numbers);
+        expect(sum).toBe(4444);
+    });
+
+    test("Calling add with a negative number will throw an exception", () => {
+        calc = new StringCalculator();
+        numbers = "//;\n12,24;25,50\n-4333;-12;25;26,-4\n-7";
+        expect(() => calc.add(numbers)).toThrow("negative numbers not allowed -4333,-12,-4,-7");
+    });
+
+
+    test('not proper input :- delimiter at end of string', () => {
+        const calc = new StringCalculator();
+        numbers = "2,-4,3,-1,,";
+        expect(() => calc.add(numbers)).toThrow("Non-numeric is not allowed!");
+    })
+
 });
 
 
-test('only one numeric value pass as string', () => {
-    const calc=new StringCalculator();
-    numbers="12";
-    const sum=calc.add(numbers);
-    expect(sum).toBe(12);
+describe("testcases 6,7,8,9,10,11,12 given in pdf",()=>{
+    
+
+
+
 });
-
-test('for non numeric values it will raise excpetion',()=>{
-    const calc=new StringCalculator();
-    expect(()=> calc.add("a") ).toThrow("Non-numeric is not allowed!");
-});
-
-test("input as two values with comma seperation",()=>{
-    const calc=new StringCalculator();
-    numbers="12,24";
-    const sum=calc.add(numbers);
-    expect(sum).toBe(36);
-});
-
-test("Allow the add method to handle any amount of numbers" ,()=>{
-    calc=new StringCalculator();
-    numbers="12,24,25,50,4333";
-    const sum=calc.add(numbers);
-    expect(sum).toBe(4444);
-});
-
-test("Allow the add method to handle new lines between numbers",()=>{
-    calc=new StringCalculator();
-    numbers="12,24\n25,50\n4333";
-    const sum=calc.add(numbers);
-    expect(sum).toBe(4444);
-});
-
-/*
-To change the delimiter, the beginning of the string will contain 
-a separate line that looks like this: "//[delimiter]\n[numbers…]".
-//  For example, "//;\n1;2" where the delimiter is ";"
-//  should return 3.
-*/
-
-
-test("Support different delimiters:",()=>{ 
-    calc=new StringCalculator();
-    numbers="//;\n12,24;25,50\n4333";
-    const sum=calc.add(numbers);
-    expect(sum).toBe(4444);
-});
-
-test("Calling add with a negative number will throw an exception",()=>{
-    calc=new StringCalculator();
-    numbers="//;\n12,24;25,50\n-4333;-12;25;26,-4\n-7";
-    expect(()=>calc.add(numbers)).toThrow("negative numbers not allowed -4333,-12,-4,-7");
-});
-
-
-test('not proper input :- delimiter at end of string',()=>{
-   const calc = new StringCalculator();
-   numbers="2,-4,3,-1,,";
-  expect(() => calc.add(numbers)).toThrow("Non-numeric is not allowed!");
-})
