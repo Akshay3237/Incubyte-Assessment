87ec1b0ef4daf55d9b0700fa5784d6b43a3f61b7
Akshay3237 <akshaygohel364@gmail.com>
2025-07-05 14:20:58 +0530

    pass testcase of Allow multiple delimiters

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 class StringCalculator {
     #counting_of_add = 0;
 
+    //return Delimiter expression so it will be easy to pass in split method
+    //i create this seperat function because of dont want all logic in one function and so then it will be easy to understand.
+    #GetDelimiter(customDelim) {
+        
+        if (customDelim[0] == '[') { //here check if there is [ or not if not then only one delim is take
+            customDelim = customDelim.substring(1, customDelim.length - 1); //remove first [ and last ] brackets
+            customDelim=customDelim
+            .split('][')
+            .map( delim => `${this.escapeRegex(delim)}`).join('|'); //now i replace ][ with / and also i use excapeRegex function
+        }
+        let delimiter = new RegExp(`\\n|,|${customDelim}`);
+        return delimiter;
+    }
 
 
     GetCalledCount() {
@@ -14,24 +27,20 @@ class StringCalculator {
 
         let negative_numbers = []; //contains negative numbers
         let delimiter = /[\n,]/;
-        
+
 
         if (numbers.startsWith("//")) {
             const delimiterLineEnd = numbers.indexOf("\n");
             let customDelim = numbers.substring(2, delimiterLineEnd);
-            if(customDelim[0]=='['){
-                customDelim=customDelim.substring(1,customDelim.length-1);
-            }
-            delimiter = new RegExp(`\\n|,|${this.escapeRegex(customDelim)}`);
+            delimiter=this.#GetDelimiter(customDelim)
             numbers = numbers.substring(delimiterLineEnd + 1);
         }
-
+       
         numbers = numbers.split(delimiter);
+       
 
 
-
-        
-      let sum = 0; //for initial value of sum as zero
+        let sum = 0; //for initial value of sum as zero
         for (let i = 0; i < numbers.length; i++) {
             if (!isNaN(parseInt(numbers[i]))) { //check if non numeric values 
                 if (numbers[i] < 0) negative_numbers.push(numbers[i]); //if number is negative then add it here
@@ -53,6 +62,8 @@ class StringCalculator {
 
         return sum; //return final sum
     }
+
+    //this function is use for if string contain any regix specific characters so it will be replace by \\character 
     escapeRegex(str) {
         return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     }
